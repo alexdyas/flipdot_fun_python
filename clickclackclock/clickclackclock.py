a2b9e6c54e6c34c1d363a9e951142d7863435302
Alexander Dyas <me@alexdyas.com>
2021-02-16 11:31:39 +0100

    All the digits

--- clickclackclock/clickclackclock.py
+++ clickclackclock/clickclackclock.py
@@ -4,6 +4,7 @@
 
 import flipdot
 from datetime import datetime
+import time
 
 flipdot = flipdot.Flipdot('/dev/ttyUSB0',57600)
 
@@ -16,8 +17,7 @@ minute = now.strftime("%M")
 print("date and time ="+hour+":"+minute)
 
 flipdot.reset(False)
-flipdot.text_letter('1',0)
-
-flipdot.display()
-
-flipdot.dumpdisplaybuffer()
+for number in range(0,10) :
+  flipdot.text_letter(str(number),0)
+  time.sleep(1)
+  flipdot.display()
