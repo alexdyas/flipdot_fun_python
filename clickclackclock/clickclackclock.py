85e25bed4e6e7f777f0ffdc008f7dee37fe544f1
Alexander Dyas <me@alexdyas.com>
2021-02-18 20:11:22 +0100

    new font

--- clickclackclock/clickclackclock.py
+++ clickclackclock/clickclackclock.py
@@ -13,22 +13,37 @@ flipdot = flipdot.Flipdot('/dev/ttyUSB0',57600)
 #flipdot.reset(False)
 #flipdot.display()
 
+
+# flipdot.text_letter("setno1","O",0)
+# flipdot.text_letter("setno1","l",7)
+# flipdot.text_letter("setno1","i",10)
+# flipdot.text_letter("setno1","v",14)
+# flipdot.display()
+
+lasthour = "00"
+lastminute = "00"
+
 while True :
   now = datetime.now()
 
   hour = now.strftime("%H")
   minute = now.strftime("%M")
 
-  flipdot.text_letter("setno1",hour[0],0)
-  flipdot.text_letter("setno1",hour[1],7)
-  flipdot.text_letter("setno1",minute[0],15)
-  flipdot.text_letter("setno1",minute[1],22)
+  if hour != lasthour :
+    flipdot.text_letter("setno1",hour[0],0)
+    flipdot.text_letter("setno1",hour[1],7)
+    flipdot.display()
+
+  if minute != lastminute :
+    flipdot.text_letter("setno1",minute[0],15)
+    flipdot.text_letter("setno1",minute[1],22)
+    flipdot.display()
 
   # !! Only send if time has changed
-
-  flipdot.display()
   time.sleep(1)
 
+  lasthour = hour
+  lastminute = minute
 
 
 #flipdot.reset(False)
